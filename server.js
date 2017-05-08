63bdeceddd9131b9d043cae1cfe2f3567cf584b1
Alex <xx01alex@yahoo.com>
2017-05-09 00:25:56 +0300

    Added pages

--- server.js
+++ server.js
@@ -9,8 +9,44 @@ app.get('/', function(req, res, next) {
     res.send(html);
 });
 
-app.get('/', function(req, res, next) {
-    var html = fs.readFileSync('views/index.html', 'utf8')
+app.get('/about-us', function(req, res, next) {
+    var html = fs.readFileSync('views/about-us.html', 'utf8')
+    res.send(html);
+});
+
+app.get('/platforms', function(req, res, next) {
+    var html = fs.readFileSync('views/platforms.html', 'utf8')
+    res.send(html);
+});
+
+app.get('/trading-options', function(req, res, next) {
+    var html = fs.readFileSync('views/trading-options.html', 'utf8')
+    res.send(html);
+});
+
+
+app.get('/clients', function(req, res, next) {
+    var html = fs.readFileSync('views/clients.html', 'utf8')
+    res.send(html);
+});
+
+app.get('/open-account', function(req, res, next) {
+    var html = fs.readFileSync('views/open-account.html', 'utf8')
+    res.send(html);
+});
+
+app.get('/news', function(req, res, next) {
+    var html = fs.readFileSync('views/news.html', 'utf8')
+    res.send(html);
+});
+
+app.get('/bonuses', function(req, res, next) {
+    var html = fs.readFileSync('views/bonuses.html', 'utf8')
+    res.send(html);
+});
+
+app.get('/contact', function(req, res, next) {
+    var html = fs.readFileSync('views/contact.html', 'utf8')
     res.send(html);
 });
 
