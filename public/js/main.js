21033036f88727365af75616cb0e17448055ff29
Alex <xx01alex@yahoo.com>
2017-05-22 21:14:47 +0300

    Account register form

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    // Generate the owl carousel from homepage
+
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:0,
@@ -15,6 +17,29 @@ $(document).ready(function(){
                 items:1
             }
         }
-    })
+    });
+
+    // Change the action link for account form
+    // Either demo register account or request real link
+
+    $('#form-demo').on('click', function(e) {
+        // TODO: insert the link for demo account register action
+        $('#account-form').attr('action', 'demo_account_link');
+        setTimeout(function(){
+            $('#account-form').submit();
+        }, 500);
+        e.stopPropagation();
+        e.preventDefault();
+    });
+
+    $('#form-real').on('click', function(e) {
+        // TODO: insert the link for real account request action
+        $('#account-form').attr('action', 'real_account_link');
+        setTimeout(function(){
+            $('#account-form').submit();
+        }, 500);
+        e.stopPropagation();
+        e.preventDefault();
+    });
 
 });
